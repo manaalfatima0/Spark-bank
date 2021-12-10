f93ffb52892eb5762c3c755fe9730c8aa7c6747d
manaalfatima0 <89380634+manaalfatima0@users.noreply.github.com>
2021-12-10 15:38:07 +0530

    Update Indian_bank.sql

--- Bank/sql/Indian_bank.sql
+++ Bank/sql/Indian_bank.sql
@@ -31,25 +31,6 @@ CREATE TABLE `users` (
   `email` varchar(30) NOT NULL,
   `balance` int(8) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `name`, `email`, `balance`) VALUES
-(1, 'Salman', 'khan@gmail.com', 50000),
-(2, 'Kiara', 'advani@gmail.com', 30000),
-(3, 'Shahrukh', 'shah@gmail.com', 40000),
-(4, 'Priyanka', 'chopra@gmail.com', 50000),
-(5, 'Shahid', 'kapoor@gmail.com', 40000),
-(6, 'Ranbir', 'singh@gmail.com', 30000),
-(7, 'Deepika', 'padukone@gmail.com', 50000),
-(8, 'Juhi', 'chawla@gmail.com', 40000),
-(9, 'Nick', 'jonas@gmail.com', 30000),
-(10, 'Taapsee', 'pannu@gmail.com', 50000);
-
---
--- Indexes for dumped tables
 --
 
 --
